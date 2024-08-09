5042ed8f5e7fe70398d14ceb2880914b38a883e5
itsjh <93984898+itsjh1242@users.noreply.github.com>
2024-08-09 13:49:16 +0900

    test

--- src/routes/dialog.tsx
+++ src/routes/dialog.tsx
@@ -12,7 +12,7 @@ export const Dialog = () => {
     <div className="flex w-full min-h-screen justify-center items-center">
       <CustomDialog isOpen={isDialogOpened} setIsOpen={setIsDialogOpened} dialogTitle="접근 제한">
         <div className="flex flex-col">
-          <p className="text-sm text-gray-500">본 서비스는 로그인이 필요합니다.</p>
+          <p className="text-sm text-gray-500">본 서비스는 로그인이 필요합니다..</p>
         </div>
       </CustomDialog>
       <Button
