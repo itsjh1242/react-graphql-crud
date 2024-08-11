24d26923cb6d0ab142565a4110acee6e93e52a0d
itsjh <93984898+itsjh1242@users.noreply.github.com>
2024-08-11 17:49:14 +0900

    merge

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,3 +3,6 @@ export { Test } from "./test";
 export { Toast } from "./toast";
 export { Dialog } from "./dialog";
 export { DatePicker } from "./date-picker";
+
+// dayzen
+export { DayzenHome } from "./dayzen/home";
