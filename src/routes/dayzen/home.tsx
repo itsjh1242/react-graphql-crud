a40f9211701b0d9298e11c90061e8ab8505b054a
itsjh <93984898+itsjh1242@users.noreply.github.com>
2024-08-12 16:37:32 +0900

    split input 진행 중

--- src/routes/dayzen/home.tsx
+++ src/routes/dayzen/home.tsx
@@ -1,3 +1,9 @@
+import { MultiSearch } from "@/components/custom-input";
+
 export const DayzenHome = () => {
-  return <div></div>;
+  return (
+    <div className="px-24">
+      <MultiSearch />
+    </div>
+  );
 };
